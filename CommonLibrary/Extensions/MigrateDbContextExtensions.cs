f86f94c4b656da84aa7fcc4b7b25c64b1b123486
Tian <jacky841224j@gmail.com>
2024-07-25 18:24:54 +0800

    Feat:新增 預設管理員身分及帳號功能

---
+++ CommonLibrary/Extensions/MigrateDbContextExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommonLibrary.Extensions
+{
+    public static class MigrateDbContextExtensions
+    {
+        public static async Task MigrateDbContextAsync<TContext>(this WebApplicationBuilder builder,
+            Func<TContext, IServiceProvider, Task> seeder) where TContext : DbContext
+        {
+            var services = builder.Services.BuildServiceProvider();
+            var context = services.GetRequiredService<TContext>();
+            if ((await context.Database.GetPendingMigrationsAsync()).Any())
+            {
+                await context.Database.MigrateAsync();
+            }
+            await seeder(context, services);
+        }
+    }
+}
---
+++ CommonLibrary/Infrastructure/SeedData.cs
@@ -0,0 +1,45 @@
+﻿using CommonLibrary.Data;
+using CommonLibrary.Entities;
+
+namespace CommonLibrary.Infrastructure
+{
+    public class SeedData
+    {
+        public async Task SeedAsync(MysqlDbContext context, IServiceProvider serviceProvider)
+        {
+
+            //新增預設角色
+            if (!context.Set<Role>().Any(u => u.Name == "admin"))
+            {
+                context.Role.Add(new Role { Name = "admin" });
+                await context.SaveChangesAsync();
+            }
+
+            var roleId = context.Role.Where(x => x.Name == "admin").Select(x => x.Id).FirstOrDefault();
+
+            if (roleId == 0)
+                throw new Exception("新增預設角色失敗");
+
+            //新增預設帳號
+            if (!context.Set<AdminUser>().Any(u => u.Account == "admin"))
+            {
+                context.AdminUser.Add(new AdminUser
+                {
+                    Account = "admin",
+                    Password = "!Allpower123",
+                    Email = "admin@allpower.in",
+                    Photo = "",
+                    RoleId = roleId
+                });
+                await context.SaveChangesAsync();
+            }
+
+            
+            var adminUser = context.AdminUser.Where(x => x.Account == "admin").Select(x => x.Id).FirstOrDefault();
+
+            if (adminUser == 0)
+                throw new Exception("新增預設帳號失敗");
+        }
+    }
+
+}

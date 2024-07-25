5fdbcd914462bea504db396e58fddf3cbb99d3e2
Tian <jacky841224j@gmail.com>
2024-07-25 18:24:01 +0800

    Feat:新增UserDto

---
+++ CommonLibrary/DTOs/User/AddUserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CommonLibrary.DTOs.User
+{
+    public class AddUserDto
+    {
+        /// <summary>
+        /// 帳號
+        /// </summary>
+        public string Account { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 使用者名稱
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ CommonLibrary/DTOs/User/GetUserDto.cs
@@ -0,0 +1,29 @@
+﻿using CommonLibrary.Enums;
+
+namespace CommonLibrary.DTOs.User
+{
+    public class GetUserDto
+    {
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// 帳號
+        /// </summary>
+        public string? Account { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 使用者名稱
+        /// </summary>
+        public string? Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 狀態 0 = 關閉, 1 = 開啟, 2 = 停用
+        /// </summary>
+        public StatusEnum? Status { get; set; }
+
+        /// <summary>
+        /// 最後登入時間
+        /// </summary>
+        public DateTime? LoginTime { get; set; }
+    }
+}

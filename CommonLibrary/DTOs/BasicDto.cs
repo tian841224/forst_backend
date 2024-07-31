88ced6ff7139dbd1bae5ba80e240d7448cb1993b
Tian <jacky841224j@gmail.com>
2024-07-31 14:24:23 +0800

    Feat:新增BasicDto

---
+++ CommonLibrary/DTOs/BasicDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonLibrary.DTOs
+{
+    public class BasicDto
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}

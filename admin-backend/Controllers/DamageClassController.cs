8de49ea87ec9b0aa876e9ea9e079335146ae06b2
Tian <jacky841224j@gmail.com>
2024-07-31 13:39:52 +0800

    Feat:新增 危害種類 功能

---
+++ admin-backend/Controllers/DamageClassController.cs
@@ -0,0 +1,82 @@
+﻿using admin_backend.Services;
+using CommonLibrary.DTOs.DamageClass;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace admin_backend.Controllers
+{
+    /// <summary>
+    /// 危害種類
+    /// </summary>
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class DamageClassController : ControllerBase
+    {
+        private readonly DamageClassService _damageClassService;
+
+        public DamageClassController(DamageClassService damageClassService)
+        {
+            _damageClassService = damageClassService;
+        }
+
+        /// <summary>
+        /// 取得樹木基本資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _damageClassService.Get(id));
+        }
+
+        /// <summary>
+        /// 取得全部樹木基本資料
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _damageClassService.Get());
+        }
+
+        /// <summary>
+        /// 新增樹木基本資料
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Add(AddDamageClassDto dto)
+        {
+            return Ok(await _damageClassService.Add(dto));
+        }
+
+        /// <summary>
+        /// 更新危害種類
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Update(int id, UpdateDamageClassDto dto)
+        {
+            return Ok(await _damageClassService.Update(id,dto));
+        }
+
+        /// <summary>
+        /// 刪除樹木基本資料
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _damageClassService.Delete(id));
+        }
+    }
+}
---
+++ admin-backend/Services/DamageClassService.cs
@@ -0,0 +1,122 @@
+﻿using CommonLibrary.Data;
+using CommonLibrary.DTOs.DamageClass;
+using CommonLibrary.DTOs.OperationLog;
+using CommonLibrary.Entities;
+using CommonLibrary.Enums;
+using CommonLibrary.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+
+namespace admin_backend.Services
+{
+    public class DamageClassService
+    {
+        private readonly ILogger<DamageClassService> _log;
+        private readonly MysqlDbContext _context;
+        private readonly OperationLogService _operationLogService;
+        public DamageClassService(ILogger<DamageClassService> log, MysqlDbContext context, OperationLogService operationLogService)
+        {
+            _log = log;
+            _context = context;
+            _operationLogService = operationLogService;
+        }
+
+        public async Task<List<DamageClass>> Get(int? Id = null)
+        {
+            if (Id.HasValue)
+                return await _context.DamageClass.Where(x => x.Id == Id).ToListAsync();
+            else
+                return await _context.DamageClass.ToListAsync();
+        }
+
+        public async Task<DamageClass> Add(AddDamageClassDto dto)
+        {
+
+            var damageClasses = new DamageClass
+            {
+                Name = dto.Name,
+                DamageTypeId = dto.DamageTypeId,
+                Status = dto.Status,
+            };
+
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            await _context.DamageClass.AddAsync(damageClasses);
+
+            //新增操作紀錄
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                await _operationLogService.Add(new AddOperationLogDto
+                {
+                    Type = ChangeTypeEnum.Add,
+                    Content = $"新增危害種類：{damageClasses.Name}",
+                });
+            };
+            scope.Complete();
+            return damageClasses;
+        }
+
+        public async Task<DamageClass> Update(int Id, UpdateDamageClassDto dto)
+        {
+
+            var damageClasses = await _context.DamageClass.Where(x => x.Id == Id).FirstOrDefaultAsync();
+
+            if (damageClasses == null)
+            {
+                throw new ApiException($"無此資料-{Id}");
+            }
+
+            if (!string.IsNullOrEmpty(dto.Name))
+                damageClasses.Name = dto.Name;
+
+            if (dto.DamageTypeId.HasValue)
+                damageClasses.DamageTypeId = dto.DamageTypeId.Value;
+
+            if (dto.Status.HasValue)
+                damageClasses.Status = dto.Status.Value;
+
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            await _context.DamageClass.AddAsync(damageClasses);
+
+            //新增操作紀錄
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                await _operationLogService.Add(new AddOperationLogDto
+                {
+                    Type = ChangeTypeEnum.Edit,
+                    Content = $"修改危害種類：{damageClasses.Name}",
+                });
+            };
+            scope.Complete();
+            return damageClasses;
+        }
+
+        public async Task<DamageClass> Delete(int Id)
+        {
+
+            var damageClasses = await _context.DamageClass.Where(x => x.Id == Id).FirstOrDefaultAsync();
+
+            if (damageClasses == null)
+            {
+                throw new ApiException($"無此資料-{Id}");
+            }
+
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            _context.DamageClass.Remove(damageClasses);
+
+            //新增操作紀錄
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                await _operationLogService.Add(new AddOperationLogDto
+                {
+                    Type = ChangeTypeEnum.Delete,
+                    Content = $"刪除危害種類：{damageClasses.Name}",
+                });
+            };
+            scope.Complete();
+            return damageClasses;
+        }
+    }
+}

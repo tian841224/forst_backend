e6cde36fff1f6bc8eeb92c3858a71e3d2e0208a8
Tian <jacky841224j@gmail.com>
2024-08-01 22:39:39 +0800

    Feat:AutoMap 改為自動映射

--- admin-backend/Extensions/MappingProfile .cs
+++ admin-backend/Extensions/MappingProfile .cs
@@ -1,5 +1,4 @@
-﻿using admin_backend.Services;
-using AutoMapper;
+﻿using AutoMapper;
 
 namespace admin_backend.Extensions
 {
@@ -7,33 +6,24 @@ namespace admin_backend.Extensions
     {
         public MappingProfile()
         {
-            // 自動映射所有屬性名稱相同的類
-            CreateMap<CommonLibrary.Entities.AdminUser, CommonLibrary.DTOs.AdminUser.AdminUserResponse>()
-                .ReverseMap();
-            CreateMap<CommonLibrary.Entities.DamageClass, CommonLibrary.DTOs.DamageClass.DamageClassResponse>()
-             .ReverseMap();
-            CreateMap<CommonLibrary.Entities.DamageType, CommonLibrary.DTOs.DamageType.DamageTypeResponse>()
-              .ReverseMap();
-            CreateMap<CommonLibrary.Entities.Documentation, CommonLibrary.DTOs.Documentation.DocumentationResponse>()
-              .ReverseMap(); 
-            CreateMap<CommonLibrary.Entities.EpidemicSummary, CommonLibrary.DTOs.EpidemicSummary.EpidemicSummaryResponse>()
-              .ReverseMap(); 
-            CreateMap<CommonLibrary.Entities.ForestCompartmentLocation, CommonLibrary.DTOs.ForestCompartmentLocation.ForestCompartmentLocationResponse>()
-              .ReverseMap();
-            CreateMap<CommonLibrary.Entities.ForestDiseasePublications, CommonLibrary.DTOs.ForestDiseasePublications.ForestDiseasePublicationsResponse>()
-              .ReverseMap(); 
-            CreateMap<CommonLibrary.Entities.MailConfig, CommonLibrary.DTOs.MailConfig.MailConfigResponse>()
-              .ReverseMap(); 
-            CreateMap<CommonLibrary.Entities.OperationLog, CommonLibrary.DTOs.OperationLog.OperationLogResponse>()
-              .ReverseMap();
-            CreateMap<CommonLibrary.Entities.RolePermission, CommonLibrary.DTOs.RolePermission.RolePermissionResponse>()
-              .ReverseMap(); 
-            CreateMap<CommonLibrary.Entities.Role, CommonLibrary.DTOs.Role.RoleResponse>()
-              .ReverseMap();
-            CreateMap<CommonLibrary.Entities.TreeBasicInfo, CommonLibrary.DTOs.TreeBasicInfo.TreeBasicInfoResponse>()
-              .ReverseMap();
-            CreateMap<CommonLibrary.Entities.User, CommonLibrary.DTOs.User.UserResponse>()
-              .ReverseMap();
+            //自動映射
+            var assembly = typeof(CommonLibrary.Entities.AdminUser).Assembly;
+
+            var entityTypes = assembly.GetTypes()
+                .Where(t => t.Namespace == "CommonLibrary.Entities" && !t.IsAbstract && !t.IsInterface)
+                .ToList();
+
+            foreach (var entityType in entityTypes)
+            {
+                var dtoTypeName = $"CommonLibrary.DTOs.{entityType.Name}.{entityType.Name}Response";
+                var dtoType = assembly.GetType(dtoTypeName);
+
+                if (dtoType != null)
+                {
+                    CreateMap(entityType, dtoType).ReverseMap();
+                }
+            }
+
             // 對某個命名空間下的所有類進行映射
             this.RecognizePrefixes("CommonLibrary.Entities", "CommonLibrary.DTOs");
             this.RecognizeDestinationPrefixes("CommonLibrary.DTOs");

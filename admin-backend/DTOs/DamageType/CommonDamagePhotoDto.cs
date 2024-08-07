a657dd8db35c7df531f3ee291d58caa70712d4cd
Tian <jacky841224j@gmail.com>
2024-08-08 02:31:25 +0800

    Feat新增常見病蟲害 PhotoDto

---
+++ admin-backend/DTOs/DamageType/CommonDamagePhotoDto.cs
@@ -0,0 +1,11 @@
+﻿using CommonLibrary.DTOs;
+
+namespace admin_backend.DTOs.DamageType
+{
+    public class CommonDamagePhotoDto : SortDto
+    {
+        public IFormFile Photo { get; set; }
+
+        //public SortDto SortDtos { get; set; } = new();
+    }
+}

8f41022d1bbb14d1982dde2ccd9dc636b3a2ebd7
Tian <jacky841224j@gmail.com>
2024-08-13 15:49:55 +0800

    Fix: 修正 新增 常見問題時為等待判斷錯誤

--- admin-backend/Services/CommonDamageService.cs
+++ admin-backend/Services/CommonDamageService.cs
@@ -155,12 +155,12 @@ namespace admin_backend.Services
         {
             await using var _context = await _contextFactory.CreateDbContextAsync();
 
-            var damageClass = _context.DamageClass.Where(x => x.Id == dto.DamageClassId).AnyAsync();
-            if (damageClass == null)
+            var damageClass = await _context.DamageClass.Where(x => x.Id == dto.DamageClassId).AnyAsync();
+            if (damageClass == null || !damageClass)
                 throw new ApiException($"此危害種類不存在-{dto.DamageClassId}");
 
-            var damageType = _context.DamageType.Where(x => x.Id == dto.DamageTypeId).AnyAsync();
-            if (damageType == null)
+            var damageType = await _context.DamageType.Where(x => x.Id == dto.DamageTypeId).AnyAsync();
+            if (damageType == null || !damageType)
                 throw new ApiException($"此危害類型不存在-{dto.DamageTypeId}");
 
             ////上傳圖片

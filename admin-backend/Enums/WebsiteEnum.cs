271cec2dcf868f9b6d020f6704af60ca35687354
Tian <jacky841224j@gmail.com>
2024-08-15 00:17:04 +0800

    Feat:增加Enum 註解

--- admin-backend/Enums/WebsiteEnum.cs
+++ admin-backend/Enums/WebsiteEnum.cs
@@ -13,18 +13,21 @@ namespace admin_backend.Enums
         /// 未設定
         /// </summary>
         [EnumMember(Value = "未設定")]
+        [Description("未設定")]
         None = 0,
 
         /// <summary>
         /// 林業自然保育署
         /// </summary>
         [EnumMember(Value = "林業自然保育署")]
+        [Description("林業自然保育署")]
         Forest = 1,
 
         /// <summary>
         /// 林業試驗所
         /// </summary>
         [EnumMember(Value = "林業試驗所")]
+        [Description("林業試驗所")]
         TFRI = 2,
     }
 }

73e63bda3cf05b7d01d771b317c06d105a060f05
Tian <jacky841224j@gmail.com>
2024-08-18 03:07:14 +0800

    Fix: 修正 危害部位 Enum 錯誤

--- admin-backend/Enums/DamagedPartEnum.cs
+++ admin-backend/Enums/DamagedPartEnum.cs
@@ -5,24 +5,32 @@ namespace admin_backend.Enums
 {
     public enum DamagedPartEnum
     {
-        [EnumMember(Value = "水泥面")]
-        [Description("水泥面")]
-        CementSurface = 1,
+        [EnumMember(Value = "葉")]
+        [Description("葉")]
+        Leaf = 1,
 
-        [EnumMember(Value = "柏油面")]
-        [Description("柏油面")]
-        AsphaltSurface = 2,
+        [EnumMember(Value = "花")]
+        [Description("花")]
+        Flower = 2,
 
-        [EnumMember(Value = "植被泥土面 (地表有草皮或鬆潤木)")]
-        [Description("植被泥土面 (地表有草皮或鬆潤木)")]
-        VegetationSoilSurface = 3,
+        [EnumMember(Value = "枝")]
+        [Description("枝")]
+        Branch = 3,
 
-        [EnumMember(Value = "花台內")]
-        [Description("花台內")]
-        FlowerBed = 4,
+        [EnumMember(Value = "幹")]
+        [Description("幹")]
+        Trunk = 4,
 
-        [EnumMember(Value = "人工鋪面 (水泥面、柏油面以外)")]
-        [Description("人工鋪面 (水泥面、柏油面以外)")]
-        ArtificialSurface = 5
+        [EnumMember(Value = "葉部")]
+        [Description("葉部")]
+        LeafPart = 5,
+
+        [EnumMember(Value = "侵害土壤部")]
+        [Description("侵害土壤部")]
+        SoilDamage = 6,
+
+        [EnumMember(Value = "全面異常症狀病害")]
+        [Description("全面異常症狀病害")]
+        OverallAbnormalDisease = 7
     }
 }

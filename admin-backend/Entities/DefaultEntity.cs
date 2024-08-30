f35d0ac5b2358ce29f454337c61284614c7c8b00
Tian <jacky841224j@gmail.com>
2024-08-30 17:36:35 +0800

    Fix:修正預設Entity複寫預設時間錯誤

--- admin-backend/Entities/DefaultEntity.cs
+++ admin-backend/Entities/DefaultEntity.cs
@@ -12,16 +12,15 @@ namespace admin_backend.Entities
         /// <summary>
         /// 建立日期
         /// </summary>
-        [Required]
         [Comment("建立日期")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public DateTime CreateTime { get;  set; } = DateTime.Now;
+        public DateTime CreateTime { get;  set; } 
 
         /// <summary>
         /// 更新時間
         /// </summary>
         [Comment("更新時間")]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public DateTime UpdateTime { get;  set; } = DateTime.Now;
+        public DateTime UpdateTime { get;  set; }
     }
 }

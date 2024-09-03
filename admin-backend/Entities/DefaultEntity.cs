d3f8ce94bdd1cfb996e7dde09365efef5a23a14b
Tian <jacky841224j@gmail.com>
2024-09-03 23:13:30 +0800

    Feat:調整DefaultEntity位置

---
+++ admin-backend/Entities/DefaultEntity.cs
@@ -0,0 +1,17 @@
+﻿using CommonLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace admin_backend.Entities
+{
+    public class DefaultEntity : BaseDefaultEntity
+    {
+        [Comment("建立日期")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override DateTime CreateTime { get; set; } = DateTime.Now;
+
+        [Comment("更新時間")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public override DateTime UpdateTime { get; set; } = DateTime.Now;
+    }
+}
